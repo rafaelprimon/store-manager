b7cdc8917e985075aa8a1f6b029ea39154e89ae1
rafaelprimon <rafaelfernandesprimon@gmail.com>
2021-11-21 08:38:07 -0300

    cria pastas para implementar msc e faz o model de conexao com o mongodb

---
+++ models/connection.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+//  const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
+let db = null;
+
+const connection = () => (db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((conn) => {
+  db = conn.db(DB_NAME);
+  return db;
+  }));
+
+module.exports = connection;
+
+/* código de conexão do contéudo do course - apenas com alteração do localhost para rodar testes locais antes de fazer os pushs */
